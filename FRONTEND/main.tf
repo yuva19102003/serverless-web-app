de437765b03008a06af54d3f75208dd25260c615
YUVARAJ K <116803074+yuva19102003@users.noreply.github.com>
2024-03-10 14:16:39 +0530

    Update main.tf

--- FRONTEND/main.tf
+++ FRONTEND/main.tf
@@ -13,17 +13,6 @@ resource "aws_s3_bucket" "Site_Origin" {
 }
 
 
-## permission for public access
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.Site_Origin.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-
 
 ## Assign policy to allow CloudFront to reach S3 bucket
 resource "aws_s3_bucket_policy" "origin" {
